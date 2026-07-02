b1509588a7f4a7c5224d425539df4416defe5464
shuchaoxi <shuchaoxi@users.noreply.github.com>
2026-07-02 19:44:13 +0800

    chore: remove temporary translation scripts

--- _test.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-sys.stdout.reconfigure(encoding='utf-8')
-f = 'theory/README.tex'
-print(f'Testing: {f}', flush=True)
-print(f'Exists: {os.path.exists(f)}', flush=True)
-with open(f, 'r', encoding='utf-8') as fh:
-    c = fh.read()
-print(f'Length: {len(c)}', flush=True)
