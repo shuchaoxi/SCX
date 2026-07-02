d64cbef8cdbd2ae0fde7406f4ccf2c08cdc080c0
shuchaoxi <shuchaoxi@users.noreply.github.com>
2026-07-02 21:41:14 +0800

    refactor: theory/ final 英文化进行中

---
+++ extract_chinese.py
@@ -0,0 +1,19 @@
+import re, sys
+
+files = [
+    'F:/scx/theory/self_evolution/spring_convergence_analysis.tex',
+    'F:/scx/theory/self_evolution/spring_hostile_review.tex',
+    'F:/scx/theory/theorems/01_noise_detection_guarantee.tex',
+    'F:/scx/theory/theorems/02_weak_feature_failure.tex',
+    'F:/scx/theory/theorems/03_unidentifiability_theorem.tex',
+]
+
+all_chinese = set()
+for fpath in files:
+    with open(fpath, 'r', encoding='utf-8') as f:
+        text = f.read()
+    for m in re.finditer(r'[\u4e00-\u9fff]{2,}', text):
+        all_chinese.add(m.group())
+
+for phrase in sorted(all_chinese, key=len, reverse=True):
+    print(phrase)
